0f17a0da7c01c55a12be9a2e38468a8a0a6e57d7
josuemj <josuemj456@gmail.com>
2023-01-16 23:54:40 -0600

    GUI preview done, no logic applied yet.

--- src/MainRadio.java
+++ src/MainRadio.java
@@ -8,12 +8,29 @@ public class MainRadio extends JFrame {
     private JButton swithOn;
     private JPanel mainRadioStage;
     private JButton switchOff;
+    private JButton em1;
+    private JButton a2Button;
+    private JButton a3Button;
+    private JButton a4Button;
+    private JButton a5Button;
+    private JButton a6Button;
+    private JButton a7Button;
+    private JButton a8Button;
+    private JButton a9Button;
+    private JButton a10Button;
+    private JButton a11Button;
+    private JButton a12Button;
+    private JButton button1;
+    private JButton button2;
+    private JComboBox comboBox1;
 
     public MainRadio(){
         setContentPane(radioForm);
         setSize(300,300);
         setTitle("Radio");
         switchOff.setVisible(false);
+        mainRadioStage.setVisible(false);
+
 
         //SwitchOn action
         swithOn.addActionListener(new ActionListener() {
